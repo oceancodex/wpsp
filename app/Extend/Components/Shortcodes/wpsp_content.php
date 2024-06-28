927b7c051fadde4c30f659766f426843f2a6ca0d
Khánh Duy Nguyễn <khanhpkvn@gmail.com>
2024-06-28 22:37:56 +0700

    KDN-PC-02: Update

--- app/Extend/Components/Shortcodes/wpsp_content.php
+++ app/Extend/Components/Shortcodes/wpsp_content.php
@@ -11,9 +11,11 @@ class wpsp_content extends BaseShortcode {
 	use InstancesTrait;
 
 	public function init($atts, $content, $tag): string {
-		if (isset($atts['id'])) {
-			$content = get_post($atts['id']);
-			$content = $content->post_content;
+		if (isset($atts['id']) && $atts['id']) {
+			$post = get_post($atts['id']);
+			if (!empty($post)) {
+				$content = $content->post_content;
+			}
 		}
 		return do_shortcode($content);
 	}
