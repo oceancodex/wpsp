5e93c66790ab8f9707d2db7b1c61ed7d6b9feea7
Khanh Duy Nguyen <khanhpkvn@gmail.com>
2024-07-31 15:28:27 +0700

    KDN-PC-01: Update

--- cli-config.php
+++ cli-config.php
@@ -12,33 +12,38 @@ use WPSP\Funcs;
 use WPSPCORE\Environment\Environment;
 use WPSPCORE\Migration\TablePrefix;
 
-/**
- * Environment.
- */
-Environment::load(__DIR__ . '/');
+try {
+	/**
+	 * Environment.
+	 */
+	Environment::load(__DIR__ . '/');
 
-/**
- * Setups.
- */
-$paths     = [__DIR__ . '/app/Entities', __DIR__ . '/database/migrations'];
-$config    = new PhpFile(__DIR__ . '/config/migrations.php');
-$isDevMode = Funcs::config('app.env') == 'dev' || Funcs::config('app.env') == 'local';
+	/**
+	 * Setups.
+	 */
+	$paths     = [__DIR__ . '/app/Entities', __DIR__ . '/database/migrations'];
+	$config    = new PhpFile(__DIR__ . '/config/migrations.php');
+	$isDevMode = Funcs::config('app.env') == 'dev' || Funcs::config('app.env') == 'local';
 
-$tablePrefix      = new TablePrefix(Funcs::instance()->_getDBTablePrefix());
-$connectionParams = include __DIR__ . '/config/migrations-db.php';
+	$tablePrefix      = new TablePrefix(Funcs::instance()->_getDBTablePrefix());
+	$connectionParams = include __DIR__ . '/config/migrations-db.php';
 
-$eventManager = new EventManager();
-$eventManager->addEventListener(Events::loadClassMetadata, $tablePrefix);
+	$eventManager = new EventManager();
+	$eventManager->addEventListener(Events::loadClassMetadata, $tablePrefix);
 
-$ORMConfig = ORMSetup::createAttributeMetadataConfiguration($paths, $isDevMode);
+	$ORMConfig = ORMSetup::createAttributeMetadataConfiguration($paths, $isDevMode);
 
-$connection = DriverManager::getConnection($connectionParams);
-$connection->getConfiguration()->setSchemaAssetsFilter(static function(string $className): bool {
-	return preg_match('/^' . Funcs::instance()->_getDBTablePrefix() . '((?!cm_cache_items))/iu', $className);
-});
+	$connection = DriverManager::getConnection($connectionParams);
+	$connection->getConfiguration()->setSchemaAssetsFilter(static function(string $className): bool {
+		return preg_match('/^' . Funcs::instance()->_getDBTablePrefix() . '((?!cm_cache_items))/iu', $className);
+	});
 
-$entityManager         = new EntityManager($connection, $ORMConfig, $eventManager);
-$existingEntityManager = new ExistingEntityManager($entityManager);
+	$entityManager         = new EntityManager($connection, $ORMConfig, $eventManager);
+	$existingEntityManager = new ExistingEntityManager($entityManager);
 
-$dependencyFactory = DependencyFactory::fromEntityManager($config, $existingEntityManager);
-return $dependencyFactory;
+	$dependencyFactory = DependencyFactory::fromEntityManager($config, $existingEntityManager);
+	return $dependencyFactory;
+}
+catch (\Exception|\Throwable $e) {
+    die($e->getMessage());
+}
