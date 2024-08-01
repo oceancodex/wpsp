a4ed18eddad7b746c619d92fc8beb78df8a942bb
Khanh Duy Nguyen <khanhpkvn@gmail.com>
2024-08-01 16:36:12 +0700

    KDN-PC-01: Update

--- resources/views/modules/admin-pages/header.php
+++ resources/views/modules/admin-pages/header.php
@@ -1,5 +1,5 @@
 <div class="wrap">
     <h1><?php echo $title ?? ''; ?></h1>
     <?php echo $notice ?? '' ?>
-    <?php include(wpsp_resources_path('/views/modules/admin-pages/wpsp/navigation.php')) ?>
+    <?php if (isset($navigation)) { include($navigation); } ?>
     <div class="wpsp-admin-page-content">
--- resources/views/modules/admin-pages/wpsp/main.php
+++ resources/views/modules/admin-pages/wpsp/main.php
@@ -1,21 +1,21 @@
 <?php
-if ($requestParams['tab'] == 'license') {
+if (isset($requestParams['tab']) && $requestParams['tab'] == 'license') {
 	$title = wpsp_trans('License key', true);
 	$view  = wpsp_resources_path('/views/modules/admin-pages/wpsp/license.php');
 }
-elseif ($requestParams['tab'] == 'database') {
+elseif (isset($requestParams['tab']) && $requestParams['tab'] == 'database') {
 	$title = wpsp_trans('Database', true);
 	$view  = wpsp_resources_path('/views/modules/admin-pages/wpsp/database.php');
 }
-elseif ($requestParams['tab'] == 'settings') {
+elseif (isset($requestParams['tab']) && $requestParams['tab'] == 'settings') {
 	$title = wpsp_trans('Settings', true);
 	$view  = wpsp_resources_path('/views/modules/admin-pages/wpsp/settings.php');
 }
-elseif ($requestParams['tab'] == 'tools') {
+elseif (isset($requestParams['tab']) && $requestParams['tab'] == 'tools') {
 	$title = wpsp_trans('Tools', true);
 	$view  = wpsp_resources_path('/views/modules/admin-pages/wpsp/tools.php');
 }
-elseif ($requestParams['tab'] == 'table') {
+elseif (isset($requestParams['tab']) && $requestParams['tab'] == 'table') {
 	$title = wpsp_trans('Table', true);
 	$view  = wpsp_resources_path('/views/modules/admin-pages/wpsp/table.php');
 }
@@ -23,6 +23,7 @@ else {
 	$title = wpsp_trans('Dashboard', true);
 	$view  = wpsp_resources_path('/views/modules/admin-pages/wpsp/dashboard.php');
 }
+$navigation = wpsp_resources_path('/views/modules/admin-pages/wpsp/navigation.php');
 
 include wpsp_resources_path('/views/modules/admin-pages/header.php');
 include $view;
