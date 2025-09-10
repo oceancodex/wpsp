c6f8ba39a8ca15797387904ffabd0ee2f89d81f5
Khánh Duy Nguyễn <khanhpkvn@gmail.com>
2025-09-10 13:32:23 +0700

    KDN-PC-01: Update

--- webpack.mix.js
+++ webpack.mix.js
@@ -7,15 +7,22 @@ const RemoveEmptyScriptsPlugin = require('webpack-remove-empty-scripts');
 
 mix.disableNotifications();
 
-glob.globSync('resources/scss/**/*.scss').forEach(file => {
-    let filePath = `public/css/${file.replace(`resources\\scss\\`, '').replace('.scss', '.min.css')}`;
+glob.sync('resources/css/**/*.css').forEach(file => {
+	let filePath = `public/css/${file.replace(`resources\\css\\`, '').replace('.css', '.min.css')}`;
+	mix.css(file, filePath).options({
+		processCssUrls: false
+	});
+});
+
+glob.sync('resources/scss/**/*.scss').forEach(file => {
+    let filePath = `public/scss/${file.replace(`resources\\scss\\`, '').replace('.scss', '.min.css')}`;
     mix.sass(file, filePath).options({
         processCssUrls: false
     });
 });
 
-glob.globSync('resources/ts/**/*.ts').forEach(file => {
-    let filePath = `public/js/${file.replace(`resources\\ts\\`, '').replace('.ts', '.min.js')}`;
+glob.sync('resources/ts/**/*.ts').forEach(file => {
+    let filePath = `public/ts/${file.replace(`resources\\ts\\`, '').replace('.ts', '.min.js')}`;
     mix.ts(file, filePath)
         .webpackConfig({
             module:  {
@@ -31,4 +38,9 @@ glob.globSync('resources/ts/**/*.ts').forEach(file => {
                 extensions: [".*", ".wasm", ".mjs", ".js", ".jsx", ".json", ".ts", ".tsx", ".vue"],
             },
         });
+});
+
+glob.sync('resources/js/**/*.js').forEach(file => {
+	let filePath = `public/js/${file.replace(/^resources[\\/]+js[\\/]+/, "").replace(".js", ".min.js")}`;
+    mix.js(file, filePath);
 });
