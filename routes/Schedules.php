ad7af07f9a8c4954cc47bdbc9498e5a56afebee7
khanh <khanhpkvn@gmail.com>
2025-09-17 05:30:30 +0700

    KDN-PC-02: Update

--- routes/Schedules.php
+++ routes/Schedules.php
@@ -17,7 +17,7 @@ class Schedules extends BaseRoute {
 	 */
 
 	public function schedules(): void {
-//		$this->schedule('wpsp_check_license', 'hourly', [CheckLicenseSchedule::class, 'init']);
+		$this->schedule('wpsp_check_license', 'hourly', [CheckLicenseSchedule::class, 'init']);
 	}
 
 	/*
@@ -25,7 +25,7 @@ class Schedules extends BaseRoute {
 	 */
 
 	public function intervals(): void {
-//		$this->interval('every_minute', 60, 'Every minute');
+		$this->interval('every_minute', 60, 'Every minute');
 	}
 
 	/*
