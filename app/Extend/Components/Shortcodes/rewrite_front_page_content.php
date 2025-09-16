39ed53092aa4726022750ab6239376c96e53d782
khanh <khanhpkvn@gmail.com>
2025-09-16 21:28:11 +0700

    KDN-PC-02: Update

--- app/Extend/Components/Shortcodes/rewrite_front_page_content.php
+++ app/Extend/Components/Shortcodes/rewrite_front_page_content.php
@@ -16,13 +16,13 @@ class rewrite_front_page_content extends BaseShortcode {
 	 *
 	 */
 
-	public function index($atts, $content, $tag): string {
+	public function index($atts, $content, $tag): \Illuminate\Contracts\View\View|string {
 //		global $wp_query;
 //		echo '<pre>'; print_r($wp_query->query_vars); echo '</pre>';
 		$rewriteIdent = get_query_var(Funcs::config('app.short_name') . '_rewrite_ident');
 		if ($rewriteIdent) {
 			$page = str_replace('_', '-', $rewriteIdent);
-			return Funcs::view('modules.rewrite-front-pages.' . $page);
+			return Funcs::view('modules.rewrite-front-pages.' . $page, ['request' => $this->request]);
 		}
 		return 'Rewrite front page content...';
 	}
