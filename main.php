1a328aba2b8e4a0ac77eb92093559179e652d3e9
khanh <khanhpkvn@gmail.com>
2025-09-18 21:37:12 +0700

    KDN-PC-02: Update

--- main.php
+++ main.php
@@ -1,10 +1,10 @@
 <?php
 /**
  * Plugin Name:         WordPress Starter Plugin
- * Description:         WordPress Starter Plugin - PHP 8.3
+ * Description:         WordPress Starter Plugin - PHP 8.4
  * Version:             1.0.0
- * Requires at least:   6.4
- * Requires PHP:        8.3
+ * Requires at least:   6.7
+ * Requires PHP:        8.4
  * Text Domain:         wpsp
  * Domain Path:         /resources/lang
  * Author:              OceanCodex
