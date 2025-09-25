5485ccd1a28450336e85733489d2dca59f90f28c
Khánh Duy Nguyễn <khanhpkvn@gmail.com>
2025-09-25 16:10:50 +0700

    KDN-PC-01: Update

--- resources/views/modules/admin-pages/wpsp/table.blade.php
+++ resources/views/modules/admin-pages/wpsp/table.blade.php
@@ -10,8 +10,8 @@
         <input type="hidden" name="page" value="{{ $_REQUEST['page'] ?? '' }}"/>
         <input type="hidden" name="tab" value="{{ $_REQUEST['tab'] ?? '' }}"/>
         @php
-            $table->views();
             $table->prepare_items();
+            $table->views();
             $table->search_box('Search', 'search_id');
             $table->display();
         @endphp
--- resources/views/modules/admin-pages/wpsp/table.php
+++ resources/views/modules/admin-pages/wpsp/table.php
@@ -3,8 +3,8 @@
     <input type="hidden" name="page" value="{{ $_REQUEST['page'] ?? '' }}"/>
     <input type="hidden" name="tab" value="{{ $_REQUEST['tab'] ?? '' }}"/>
     <?php
-        $table->views();
         $table->prepare_items();
+        $table->views();
         $table->search_box('Search', 'search_id');
         $table->display();
     ?>
