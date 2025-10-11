79d0fd66db6010491b9b4240e55ba907c0f119f0
Khánh Duy Nguyễn <khanhpkvn@gmail.com>
2025-10-11 20:08:07 +0700

    KDN-PC-01: Update

--- app/Traits/InstancesTrait.php
+++ app/Traits/InstancesTrait.php
@@ -6,4 +6,10 @@ use WPSP\Funcs;
 
 trait InstancesTrait {
 
+	protected function beforeInstanceConstruct(): void {
+		$this->mainPath      = Funcs::getInstance()->_getMainPath();
+		$this->rootNamespace = Funcs::getInstance()->_getRootNamespace();
+		$this->prefixEnv     = Funcs::getInstance()->_getPrefixEnv();
+	}
+
 }
