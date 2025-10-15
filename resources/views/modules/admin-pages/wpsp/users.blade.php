f4afcb61fdf006e78e07ef94ea1822355ea18f0a
Khánh Duy Nguyễn <khanhpkvn@gmail.com>
2025-10-15 15:32:06 +0700

    KDN-PC-01: Update

--- resources/views/modules/admin-pages/wpsp/users.blade.php
+++ resources/views/modules/admin-pages/wpsp/users.blade.php
@@ -114,6 +114,54 @@
                 </div>
             </div>
         </div>
+    @elseif($current_request->get('action') == 'add_new')
+        <form method="POST">
+            <input name="action" value="add_new_user" type="hidden"/>
+            <div id="poststuff" class="row gx-2">
+                <div class="col">
+                    <div class="meta-box-sortables ui-sortable">
+                        <div class="postbox">
+
+                            <div class="postbox-header">
+                                <h2 class="hndle ui-sortable-handle">{{ wpsp_trans('Add new user', true) }}</h2>
+                                <div class="handle-actions">
+                                    <button type="button" class="handlediv" aria-expanded="true">
+                                        <span class="toggle-indicator"></span>
+                                    </button>
+                                </div>
+                            </div>
+
+                            <div class="inside form-table w-auto">
+
+                                <div class="input-group mt-2 mb-3">
+                                    <label for="username">
+                                        {{ wpsp_trans('Username', true) }}:
+                                        <input type="text" id="username" name="username" class="w-100 mt-1" value="{{ $_POST['username'] ?? 'username_' . time() }}"/>
+                                    </label>
+                                </div>
+
+                                <div class="input-group mt-2 mb-3">
+                                    <label for="email">
+                                        {{ wpsp_trans('Email', true) }}:
+                                        <input type="text" id="email" name="email" class="w-100 mt-1" value="{{ $_POST['email'] ?? 'email_' . time() . '@example.com' }}"/>
+                                    </label>
+                                </div>
+
+                                <div class="input-group mt-2">
+                                    <label for="password">
+                                        {{ wpsp_trans('Password', true) }}:
+                                        <input type="text" id="password" name="password" class="w-100 mt-1" value="{{ $_POST['password'] ?? '123456' }}"/>
+                                    </label>
+                                </div>
+
+                            </div>
+
+                        </div>
+                        <button type="submit" class="button button-primary">{{ wpsp_trans('Add new', true) }}</button>
+                    </div>
+                </div>
+            </div>
+        </form>
     @else
         <form method="GET">
             <input type="hidden" name="page" value="{{ $_REQUEST['page'] ?? '' }}"/>
