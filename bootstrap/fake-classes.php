34ca14374025f8e001e85d096855257c5f6c88f4
Khánh Duy Nguyễn <khanhpkvn@gmail.com>
2025-10-16 14:19:59 +0700

    KDN-PC-01: Update

--- bootstrap/fake-classes.php
+++ bootstrap/fake-classes.php
@@ -12,6 +12,14 @@ if (!trait_exists('\Illuminate\Database\Eloquent\SoftDeletes')) {
 	eval('namespace Illuminate\Database\Eloquent; trait SoftDeletes {}');
 }
 
+if (!trait_exists('\WPSPCORE\Auth\Traits\VirtualAttributesTrait')) {
+	eval('namespace WPSPCORE\Auth\Traits; trait VirtualAttributesTrait {}');
+}
+
+if (!trait_exists('\WPSPCORE\Sanctum\Traits\UserSanctumTokensTrait')) {
+	eval('namespace WPSPCORE\Sanctum\Traits; trait UserSanctumTokensTrait {}');
+}
+
 if (!class_exists('\WPSPCORE\Database\Base\BaseModel')) {
 	eval('namespace WPSPCORE\Database\Base; class BaseModel {}');
 }
