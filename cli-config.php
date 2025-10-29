ca9f265d2c30e6cec745ba7d7849239168922fb3
Khánh Duy Nguyễn <khanhpkvn@gmail.com>
2025-10-29 14:18:13 +0700

    KDN-PC-01: Update

--- cli-config.php
+++ cli-config.php
@@ -44,6 +44,6 @@ try {
 	$dependencyFactory = DependencyFactory::fromEntityManager($config, $existingEntityManager);
 	return $dependencyFactory;
 }
-catch (\Exception|\Throwable $e) {
+catch (\Throwable $e) {
     die($e->getMessage());
 }
