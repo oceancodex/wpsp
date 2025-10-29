c32d6405b4754d75e75f502ac9f9734be6174b87
Khánh Duy Nguyễn <khanhpkvn@gmail.com>
2025-10-29 15:16:50 +0700

    KDN-PC-01: Update

--- app/Extras/Instances/Commands/RouteRemapCommand.php
+++ app/Extras/Instances/Commands/RouteRemapCommand.php
@@ -2,10 +2,15 @@
 
 namespace WPSP\app\Extras\Instances\Commands;
 
+use WPSP\app\Extras\Instances\Routes\MapRoutes;
 use WPSPCORE\Console\Traits\CommandsTrait;
 
 class RouteRemapCommand extends \WPSPCORE\Console\Commands\RouteRemapCommand {
 
 	use CommandsTrait;
+
+	public function customProperties() {
+		$this->mapRoutes = MapRoutes::instance();
+	}
 
 }
