63358afb69d318056ddb258fcaf6371abdec4ac3
khanh <khanhpkvn@gmail.com>
2025-10-30 23:48:22 +0700

    KDN-PC-02: Update

--- bin/scripts.php
+++ bin/scripts.php
@@ -1,27 +1,38 @@
 <?php
-$projectDir = getcwd();
-$projectName = basename($projectDir);
 
-$dirs = ['.github']; // thêm folder khác nếu muốn
+function color($text, $color = 'default') {
+	$colors = [
+		'default' => "\e[0m",
+		'red'     => "\e[31m",
+		'green'   => "\e[32m",
+		'yellow'  => "\e[33m",
+		'blue'    => "\e[34m",
+		'cyan'    => "\e[36m",
+		'bold'    => "\e[1m",
+	];
+
+	return ($colors[$color] ?? $colors['default']) . $text . $colors['default'];
+}
+
+$projectDir  = getcwd();
+$projectName = basename($projectDir);
+$dirs        = ['.github',];
 
 foreach ($dirs as $dir) {
 	if (!is_dir($dir)) continue;
-
 	$it = new RecursiveIteratorIterator(
 		new RecursiveDirectoryIterator($dir, FilesystemIterator::SKIP_DOTS),
 		RecursiveIteratorIterator::CHILD_FIRST
 	);
-
 	foreach ($it as $file)
 		$file->isDir() ? @rmdir($file) : @unlink($file);
-
 	@rmdir($dir);
-//	echo "🧹 Deleted: $dir\n";
 }
 
 if (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN') {
-	system('');
+	// Thử bật Virtual Terminal Processing nếu có
+	$ok = @shell_exec(''); // tránh warning
 }
 
 echo "\n";
-echo "\e[32m Congratulation! The plugin name: \"{$projectName}\" created successfully!\e[0m\n";
+echo color('Congratulation! The plugin "'.$projectName.'" created successfully!', 'green');
