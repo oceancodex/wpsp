b5289e0c389be67f5c0a46725a68450a1ff013ae
khanh <khanhpkvn@gmail.com>
2025-10-31 04:33:59 +0700

    KDN-PC-02: Update

--- resources/views/modules/admin-pages/header.php
+++ resources/views/modules/admin-pages/header.php
@@ -1,5 +1,5 @@
 <div class="wrap">
     <h1><?php echo $title ?? ''; ?></h1>
-    <?php echo $notice ?? '' ?>
+    <?php global $notice; echo $notice ?? '' ?>
     <?php if (isset($navigation)) { include($navigation); } ?>
     <div class="wpsp-admin-page-content">
