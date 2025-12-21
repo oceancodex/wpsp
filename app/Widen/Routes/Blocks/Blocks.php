3a471510ad6c6df41d155e0dab84375b7e43e68d
Khánh Duy Nguyễn <khanhpkvn@gmail.com>
2025-12-21 20:17:49 +0700

    KDN-PC-01: Update

---
+++ app/Widen/Routes/Blocks/Blocks.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace WPSP\App\Widen\Routes\Blocks;
+
+use WPSP\App\Widen\Traits\InstancesTrait;
+
+class Blocks extends \WPSPCORE\App\Routes\Blocks\Blocks {
+
+	use InstancesTrait;
+
+	/*
+	 *
+	 */
+
+	public function customProperties() {}
+
+}
