30e86c8a38b91d940324716c0a2d0bf5540cecca
khanh <khanhpkvn@gmail.com>
2025-12-27 02:34:29 +0700

    KDN-PC-02: Update database connection

--- app/Models/SettingsModel.php
+++ app/Models/SettingsModel.php
@@ -8,7 +8,7 @@ class SettingsModel extends Model {
 
 	use SoftDeletes;
 
-	protected $connection = 'wordpress';
+	protected $connection = 'wp_wpsp';
 //	protected $prefix     = 'wp_wpsp_';
 	protected $table      = 'settings';
 //	protected $primaryKey = 'id';
