276d27024dd763a18b4846efd647bcf49564fbbf
khanh <khanhpkvn@gmail.com>
2025-12-29 22:10:07 +0700

    KDN-PC-02: Update

--- routes/AdminPages.php
+++ routes/AdminPages.php
@@ -92,6 +92,7 @@ class AdminPages {
 				Route::get('wpsp&tab=users&action=create', [wpsp_tab_users::class, 'create'])->name('create');
 				Route::post('wpsp&tab=users&action=create', [wpsp_tab_users::class, 'store'])->name('create');
 //				Route::get('wpsp&tab=users&action=show&id=(?P<id>\w+)?&abc=(?P<abc>\w+)?', [wpsp_tab_users::class, 'show'])->name('show');
+//				Route::get('wpsp&show=(?P<user>\d+)(?P<n>&?)(?P<queries>.*)', [wpsp_tab_users::class, 'show'])->name('show');
 				Route::get('wpsp&tab=users&action=show&user_id={user_id?}', [wpsp_tab_users::class, 'show'])->name('show');
 				Route::get('wpsp&tab=users&action=edit&id=(?P<id>\d+)', [wpsp_tab_users::class, 'edit'])->middleware(AdministratorCapability::class)->name('edit');
 				Route::post('wpsp&tab=users&action=edit&id=(?P<id>\d+)', [wpsp_tab_users::class, 'update'])->middleware(AdministratorCapability::class)->name('update');
