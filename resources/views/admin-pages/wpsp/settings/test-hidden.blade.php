51c2e7abbf61f7ea16f8d1af46c4458eab7029f1
khanh <khanhpkvn@gmail.com>
2026-01-14 03:22:29 +0700

    KDN-PC-02: Update

--- resources/views/admin-pages/wpsp/settings/test-hidden.blade.php
+++ resources/views/admin-pages/wpsp/settings/test-hidden.blade.php
@@ -1,6 +1,7 @@
 <x-admin-pages.admin-page-meta-box id="testhiddendiv" :adminPageMetaBoxes="$admin_page_meta_boxes">
     <x-slot:title>Test hidden</x-slot:title>
     <x-slot:content>
-        Metabox này được ẩn mặc định
+        <p>Đây là meta box được xây dựng bằng Blade Template Engine và được gọi ra với phương thức "view".<br/>Khi sử dụng phương thức này, screen options sẽ cần được thiết lập để hiển thị ra.</p>
+        Metabox này được ẩn mặc định để thử nghiệm việc khai báo meta boxes trong admin page class với hidden meta boxes.
     </x-slot:content>
 </x-admin-pages.admin-page-meta-box>
