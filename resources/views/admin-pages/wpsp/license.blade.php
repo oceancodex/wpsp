3d43331aa31ba4f5ad67da35d02d7d3fd0f48508
Khánh Duy Nguyễn <khanhpkvn@gmail.com>
2026-02-14 13:29:59 +0700

    KDN-PC-01: Update

--- resources/views/admin-pages/wpsp/license.blade.php
+++ resources/views/admin-pages/wpsp/license.blade.php
@@ -23,7 +23,7 @@
                         </div>
                         <div class="inside">
                             <label class="screen-reader-text" for="settings[license_key]">{{ wpsp_trans('messages.license_key') }}</label>
-                            <input type="text" name="settings[license_key]" id="settings[license_key]" value="{{ $settings['license_key'] ?? '' }}" style="margin-top: 5px; width: 100%;" placeholder="xxxx-xxxx-xxxx-xxxx-xxxx"/>
+                            <input type="text" name="settings[license_key]" id="settings[license_key]" value="{{ old('settings.license_key') ?? $settings['license_key'] ?? '' }}" style="margin-top: 5px; width: 100%;" placeholder="xxxx-xxxx-xxxx-xxxx-xxxx"/>
                         </div>
                     </div>
                     <button type="submit" class="button button-primary">{{ wpsp_trans('messages.save_changes') }}</button>
