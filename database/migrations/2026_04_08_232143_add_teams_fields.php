a5f0d5b8310c7e89bfdfc647f31d3302e7395b4e
Khánh Duy Nguyễn <khanhpkvn@gmail.com>
2026-04-09 07:29:46 +0700

    KDN-PC-02: Update

---
+++ database/migrations/2026_04_08_232143_add_teams_fields.php
@@ -0,0 +1,90 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+use WPSP\Funcs;
+
+return new class extends Migration
+{
+	/**
+	 * Run the migrations.
+	 */
+	public function up(): void
+	{
+		$teams = Funcs::config('permission.teams');
+		$tableNames = Funcs::config('permission.table_names');
+		$columnNames = Funcs::config('permission.column_names');
+		$pivotRole = $columnNames['role_pivot_key'] ?? 'role_id';
+		$pivotPermission = $columnNames['permission_pivot_key'] ?? 'permission_id';
+
+		if (! $teams) {
+			return;
+		}
+
+		throw_if(empty($tableNames), 'Error: config/permission.php not loaded. Run [php artisan config:clear] and try again.');
+		throw_if(empty($columnNames['team_foreign_key'] ?? null), 'Error: team_foreign_key on config/permission.php not loaded. Run [php artisan config:clear] and try again.');
+
+		if (! Schema::hasColumn($tableNames['roles'], $columnNames['team_foreign_key'])) {
+			Schema::table($tableNames['roles'], function (Blueprint $table) use ($columnNames) {
+				$table->unsignedBigInteger($columnNames['team_foreign_key'])->nullable()->after('id');
+				$table->index($columnNames['team_foreign_key'], 'roles_team_foreign_key_index');
+
+				$table->dropUnique('roles_name_guard_name_unique');
+				$table->unique([$columnNames['team_foreign_key'], 'name', 'guard_name']);
+			});
+		}
+
+		if (! Schema::hasColumn($tableNames['model_has_permissions'], $columnNames['team_foreign_key'])) {
+			Schema::table($tableNames['model_has_permissions'], function (Blueprint $table) use ($tableNames, $columnNames, $pivotPermission) {
+				$table->unsignedBigInteger($columnNames['team_foreign_key'])->default('1');
+				$table->index($columnNames['team_foreign_key'], 'model_has_permissions_team_foreign_key_index');
+
+				if (Funcs::app('db')::getDriverName() !== 'sqlite') {
+					$table->dropForeign([$pivotPermission]);
+				}
+				$table->dropPrimary();
+
+				$table->primary([$columnNames['team_foreign_key'], $pivotPermission, $columnNames['model_morph_key'], 'model_type'],
+					'model_has_permissions_permission_model_type_primary');
+				if (Funcs::app('db')::getDriverName() !== 'sqlite') {
+					$table->foreign($pivotPermission)
+						->references('id')
+						->on($tableNames['permissions'])
+						->cascadeOnDelete();
+				}
+			});
+		}
+
+		if (! Schema::hasColumn($tableNames['model_has_roles'], $columnNames['team_foreign_key'])) {
+			Schema::table($tableNames['model_has_roles'], function (Blueprint $table) use ($tableNames, $columnNames, $pivotRole) {
+				$table->unsignedBigInteger($columnNames['team_foreign_key'])->default('1');
+				$table->index($columnNames['team_foreign_key'], 'model_has_roles_team_foreign_key_index');
+
+				if (Funcs::app('db')::getDriverName() !== 'sqlite') {
+					$table->dropForeign([$pivotRole]);
+				}
+				$table->dropPrimary();
+
+				$table->primary([$columnNames['team_foreign_key'], $pivotRole, $columnNames['model_morph_key'], 'model_type'],
+					'model_has_roles_role_model_type_primary');
+				if (Funcs::app('db')::getDriverName() !== 'sqlite') {
+					$table->foreign($pivotRole)
+						->references('id')
+						->on($tableNames['roles'])
+						->cascadeOnDelete();
+				}
+			});
+		}
+
+		Funcs::app('cache')
+			->store(Funcs::config('permission.cache.store') != 'default' ? Funcs::config('permission.cache.store') : null)
+			->forget(Funcs::config('permission.cache.key'));
+	}
+
+	/**
+	 * Reverse the migrations.
+	 */
+	public function down(): void {}
+};
