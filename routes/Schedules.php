0daf888758550295f76a32eb677a8309a3ff4b3b
Khánh Duy Nguyễn <khanhpkvn@gmail.com>
2026-04-12 23:54:20 +0700

    KDN-PC-02: Update

--- routes/Schedules.php
+++ routes/Schedules.php
@@ -3,8 +3,11 @@
 namespace WPSP\routes;
 
 use WPSP\App\Widen\Routes\Schedules\Schedules as Route;
+use WPSP\App\Widen\Support\Facades\Schedule;
 use WPSP\App\WordPress\Schedules\CheckLicenseSchedule;
+use WPSP\Funcs;
 use WPSPCORE\App\Routes\Schedules\SchedulesRouteTrait;
+use WPSP\App\WordPress\Schedules\custom_schedule;
 
 class Schedules {
 
@@ -15,7 +18,13 @@ class Schedules {
 	 */
 
 	public function schedules() {
+		// WordPress schedule system.
 		Route::schedule('wpsp_check_license', [CheckLicenseSchedule::class, 'run'], ['interval' => 'every_minute']);
+
+		// WPSP schedule system.
+		Schedule::name('WPSP')->call(function() { error_log('WPSP Schedule'); })->everyMinute();
+//		Schedule::name('custom_schedule')->call(function() { Funcs::app(custom_schedule::class)->handle(); })->everyMinute();
+//		Schedule::name('custom_schedule_run_command')->command('route:remap')->everyMinute();
 	}
 
 	/*
@@ -23,7 +32,7 @@ class Schedules {
 	 */
 
 	public function intervals() {
-		Route::interval('every_minute', 60, 'Every minute');
+		Route::interval('everyMinute', 60, 'Every minute');
 	}
 
 	/*
