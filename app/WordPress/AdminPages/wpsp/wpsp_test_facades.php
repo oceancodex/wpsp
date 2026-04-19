c85c7feff83002013a1f62ecef1b8b3f80e415d9
Khánh Duy Nguyễn <khanhpkvn@gmail.com>
2026-04-19 22:03:06 +0700

    KDN-PC-02: Update

---
+++ app/WordPress/AdminPages/wpsp/wpsp_test_facades.php
@@ -0,0 +1,183 @@
+<?php
+
+namespace WPSP\App\WordPress\AdminPages\wpsp;
+
+use Illuminate\Http\Request;
+use WPSP\App\Widen\Support\Facades\Lang;
+use WPSP\App\Widen\Support\Facades\Process;
+use WPSP\App\Widen\Support\Facades\Storage;
+use WPSP\App\Widen\Traits\InstancesTrait;
+use WPSP\Funcs;
+use WPSPCORE\App\WordPress\AdminPages\BaseAdminPage;
+
+class wpsp_test_facades extends BaseAdminPage {
+
+	use InstancesTrait;
+
+	/**
+	 * WordPress admin page properties.
+	 */
+	public $menu_title             = 'Test Facades';
+	public $page_title             = 'Test Facades';
+	public $capability             = 'manage_options';
+//	public $menu_slug              = 'wpsp-child-example';
+	public $icon_url               = 'dashicons-admin-generic';
+//	public $position               = 2;
+	public $parent_slug            = 'wpsp';
+
+	/**
+	 * Parent properties.
+	 */
+//	public $classes                = null;
+//	public $firstSubmenuTitle      = null;
+//	public $firstSubmenuClasses    = null;
+	public $isSubmenuPage          = true;
+//	public $removeFirstSubmenu     = true;
+
+//	public $urlsMatchCurrentAccess = [];
+//	public $urlsMatchHighlightMenu = [];
+
+//	public $showScreenOptions      = true;
+//	public $screenOptionsKey       = null;
+//	public $screenOptionsPageNow   = null;
+
+//	public $adminPageMetaBoxes     = [];
+
+	/**
+	 * Custom properties.
+	 */
+	private $currentTab            = null;
+	private $currentPage           = null;
+//	private $table                 = null;
+
+	/*
+	 *
+	 */
+
+	/**
+	 * Tùy biến những thuộc tính chuyên sâu\
+	 * hoặc khởi tạo các thuộc tính để tái sử dụng trong toàn bộ class.
+	 */
+	public function customProperties() {
+		/**
+		 * Xác định xem menu này sẽ được highlight khi truy cập bất cứ URL nào hay không.\
+		 * Nếu URL hiện tại khớp với một trong các item của mảng thì menu này sẽ được highlight.
+		 */
+		$this->urlsMatchHighlightMenu = [
+//			'admin.php?page=wpsp&tab=dashboard',
+		];
+
+		/**
+		 * Xác định xem menu này có đang thực sự được truy cập hay không.\
+		 * Nếu URL hiện tại khớp với một trong các item của mảng thì menu này xem như\
+		 * đang được truy cập thực sự:
+		 * - Khi đó các cài đặt liên quan đến screen options sẽ được thực thi.
+		 * - Khi đó phương thức "matchedCurrentAccess" tại đây sẽ được thực thi.
+		 *
+		 * Cần phải làm điều này để thực thi những công việc mà chỉ menu này cần.\
+		 * Chấp nhận String hoặc Regex.
+		 */
+		$this->urlsMatchCurrentAccess = [
+//			'/admin\.php\?page=wpsp&tab=dashboard/iu',
+		];
+
+		$this->currentTab  = $this->request->get('tab');
+		$this->currentPage = $this->request->get('page');
+//		$this->page_title  = ($this->currentTab ? Funcs::trans('messages.' . $this->currentTab) : Funcs::trans('messages.wpsp_child_example')) . ' - ' . Funcs::config('app.name');
+
+		/**
+		 * Định nghĩa các metaboxes sẽ được hiển thị trong admin page.
+		 */
+//		$this->adminPageMetaBoxes = [];
+
+		/**
+		 * Định nghĩa screen option key duy nhất dựa theo params trong URL.\
+		 * Ví dụ: page=wpsp&tab=list => wpsp_page_wpsp_tab_list\
+		 * Như vậy thì screen options sẽ độc lập giữa các page.
+		 */
+//		$this->screenOptionsKey = $this->funcs->_slugParams(['page', 'tab']);
+
+		/**
+		 * Ghi đè "pagenow" để gửi Ajax sắp xếp lại các metaboxes trong admin page\
+		 * và screen layout columns.
+		 */
+//		$this->screenOptionsPageNow = $this->funcs->_slugParams(['page', 'tab']);
+	}
+
+	/*
+	 *
+	 */
+
+//	public function init($path = null) {
+//		// You must call to parent method "init" if you want to custom it.
+//		parent::init();
+//
+//      // Your code here...
+//	}
+
+	public function beforeInit() {}
+
+	public function afterAddAdminPage($adminPage) {}
+
+	public function beforeLoadAdminPage($adminPage) {}
+
+	public function beforeInLoadAdminPage($adminPage) {}
+
+	public function afterInLoadAdminPage($adminPage) {}
+
+	public function afterLoadAdminPage($adminPage) {}
+
+	public function matchedCurrentAccess() {}
+
+	public function afterInit() {}
+
+	/*
+	 *
+	 */
+
+//	public function screenOptions($adminPage) {}
+
+	/*
+	 *
+	 */
+
+	public function index(Request $request) {
+		echo '<div class="wrap"><h1>Admin page: "wpsp_test_facades"</h1></div>';
+
+		// Test Lang.
+//		echo Lang::get('auth.throttle', ['seconds' => 2], 'vi');
+
+		// Test storage.
+//		Storage::disk('public')->makeDirectory('test-storage');
+
+		// Test Process.
+//		$a = Process::run('cmd /c dir'); // Windows.
+//		$a = Process::run('ls -la'); // Linux.
+//		echo '<pre style="background:white;z-index:9999;position:relative">'; print_r($a->output()); echo '</pre>'; die();
+	}
+
+	public function create(Request $request) {}
+
+	public function store(Request $request) {}
+
+	public function show(Request $request, $id) {}
+
+	public function edit(Request $request, $id) {}
+
+	public function update(Request $request) {}
+
+	public function destroy(Request $request) {}
+
+	public function forceDestroy(Request $request) {}
+
+	/*
+	 *
+	 */
+
+	public function styles() {}
+
+	public function scripts() {}
+
+	public function localizeScripts() {}
+
+}
