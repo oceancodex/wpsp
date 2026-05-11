acbf337a9e79f95f7b45a29300e0ba61399e0e8d
Khánh Duy Nguyễn <khanhpkvn@gmail.com>
2026-05-12 03:57:39 +0700

    KDN-PC-02: Update run original application via "public/index.php"

--- routes/original/web.php
+++ routes/original/web.php
@@ -3,9 +3,9 @@
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
 use WPSP\App\Http\Controllers\ApisController;
-use WPSP\App\Http\Middleware\PreventRequestForgeryWithoutOrigin;
+use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
 
 Route::get('/', function () { return view('welcome'); })->name('welcome');
 Route::get('login', function() { return view('auth.login-original'); })->name('login');
-Route::post('login', [ApisController::class, 'loginOriginal'])->middleware(PreventRequestForgeryWithoutOrigin::class);
+Route::post('login', [ApisController::class, 'loginOriginal'])->middleware(VerifyCsrfToken::class);
 Route::get('logout', function() { Auth::logout(); return redirect()->route('welcome'); });
