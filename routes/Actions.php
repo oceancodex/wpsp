756cad8b82ff6586b904ce2b9fb53c1a22aea18d
Khánh Duy Nguyễn <khanhpkvn@gmail.com>
2026-06-04 19:45:21 +0700

    KDN-PC-02: Update

--- routes/Actions.php
+++ routes/Actions.php
@@ -17,7 +17,8 @@ class Actions {
 
 	public function actions() {
 //		Route::action('wp_head', [PagesController::class, 'index']);
-//		Route::action('admin_enqueue_scripts', [AssetsController::class, 'backend']);
+		Route::action('admin_enqueue_scripts', [AssetsController::class, 'backend']);
+		Route::action('wp_enqueue_scripts', [AssetsController::class, 'frontend']);
 //		Route::action('current_screen', [PagesController::class, 'edit_user_screen']);
 	}
 
