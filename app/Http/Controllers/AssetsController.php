7f8b3e42368534f2f02d117a2e4fdeb32fb8305c
Khánh Duy Nguyễn <khanhpkvn@gmail.com>
2026-06-07 05:11:47 +0700

    KDN-PC-02: Update

--- app/Http/Controllers/AssetsController.php
+++ app/Http/Controllers/AssetsController.php
@@ -11,13 +11,17 @@ class AssetsController extends BaseController {
 //		wp_enqueue_style(config('app.short_name') . '-frontend', Funcs::asset('/css/frontend.min.css'), 9999, time());
 //		wp_enqueue_script(config('app.short_name') . '-frontend', Funcs::asset('/js/web/main.min.js'), 9999, time(), true);
 
-		wp_enqueue_script(config('app.short_name') . '-socketio', Funcs::asset('widen/plugins/socketio/socket.io.min.js'), 9999, time(), ['in_footer' => 'true']);
-		wp_enqueue_script(config('app.short_name') . '-live-reload', Funcs::asset('node/live-reload.js'), ['jquery'], time(), ['in_footer' => 'true']);
+		if (Funcs::env('WPSP_APP_DEBUG_LIVE_RELOAD') === 'true') {
+			wp_enqueue_script(config('app.short_name') . '-socketio', Funcs::asset('widen/plugins/socketio/socket.io.min.js'), 9999, time(), ['in_footer' => 'true']);
+			wp_enqueue_script(config('app.short_name') . '-live-reload', Funcs::asset('node/live-reload.js'), ['jquery'], time(), ['in_footer' => 'true']);
+		}
 	}
 
 	public function backend() {
-		wp_enqueue_script(config('app.short_name') . '-socketio', Funcs::asset('widen/plugins/socketio/socket.io.min.js'), 9999, time(), ['in_footer' => 'true']);
-		wp_enqueue_script(config('app.short_name') . '-live-reload', Funcs::asset('node/live-reload.js'), ['jquery'], time(), ['in_footer' => 'true']);
+		if (Funcs::env('WPSP_APP_DEBUG_LIVE_RELOAD') === 'true') {
+			wp_enqueue_script(config('app.short_name') . '-socketio', Funcs::asset('widen/plugins/socketio/socket.io.min.js'), 9999, time(), ['in_footer' => 'true']);
+			wp_enqueue_script(config('app.short_name') . '-live-reload', Funcs::asset('node/live-reload.js'), ['jquery'], time(), ['in_footer' => 'true']);
+		}
 
 //		wp_enqueue_script('dashboard');
 		wp_enqueue_script('postbox');
--- app/Widen/Support/Facades/Event.php
+++ app/Widen/Support/Facades/Event.php
@@ -4,13 +4,18 @@ namespace WPSP\App\Widen\Support\Facades;
 
 use WPSP\App\Widen\Traits\InstancesTrait;
 use WPSP\Funcs;
+use \WPSPCORE\App\Events\Events as EventsCore;
 
-class Event extends \WPSPCORE\App\Events\Events {
+class Event extends EventsCore {
 
 	use InstancesTrait;
 
+	/** @var EventsCore|null */
 	public static $instance  = null;
 
+	/**
+	 * @return EventsCore|null
+	 */
 	public static function instance() {
 		if (!static::$instance) {
 			$instance = new static(
