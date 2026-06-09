a1e3e4c3f556447d30b29f14b6b84f3c5f54c3a5
Khánh Duy Nguyễn <khanhpkvn@gmail.com>
2026-06-09 19:30:17 +0700

    KDN-MAC-01: Update

--- app/Http/Controllers/AssetsController.php
+++ app/Http/Controllers/AssetsController.php
@@ -8,19 +8,19 @@ use WPSPCORE\App\Http\Controllers\BaseController;
 class AssetsController extends BaseController {
 
 	public function frontend() {
-//		wp_enqueue_style(config('app.short_name') . '-frontend', Funcs::asset('/css/frontend.min.css'), 9999, time());
-//		wp_enqueue_script(config('app.short_name') . '-frontend', Funcs::asset('/js/web/main.min.js'), 9999, time(), true);
+//		wp_enqueue_style(Funcs::config('app.short_name') . '-frontend', Funcs::asset('/css/frontend.min.css'), 9999, time());
+//		wp_enqueue_script(Funcs::config('app.short_name') . '-frontend', Funcs::asset('/js/web/main.min.js'), 9999, time(), true);
 
 		if (Funcs::env('WPSP_APP_DEBUG_LIVE_RELOAD') === 'true') {
-			wp_enqueue_script(config('app.short_name') . '-socketio', Funcs::asset('widen/plugins/socketio/socket.io.min.js'), 9999, time(), ['in_footer' => 'true']);
-			wp_enqueue_script(config('app.short_name') . '-live-reload', Funcs::asset('node/live-reload.js'), ['jquery'], time(), ['in_footer' => 'true']);
+			wp_enqueue_script(Funcs::config('app.short_name') . '-socketio', Funcs::asset('widen/plugins/socketio/socket.io.min.js'), 9999, time(), ['in_footer' => 'true']);
+			wp_enqueue_script(Funcs::config('app.short_name') . '-live-reload', Funcs::asset('node/live-reload.js'), ['jquery'], time(), ['in_footer' => 'true']);
 		}
 	}
 
 	public function backend() {
 		if (Funcs::env('WPSP_APP_DEBUG_LIVE_RELOAD') === 'true') {
-			wp_enqueue_script(config('app.short_name') . '-socketio', Funcs::asset('widen/plugins/socketio/socket.io.min.js'), 9999, time(), ['in_footer' => 'true']);
-			wp_enqueue_script(config('app.short_name') . '-live-reload', Funcs::asset('node/live-reload.js'), ['jquery'], time(), ['in_footer' => 'true']);
+			wp_enqueue_script(Funcs::config('app.short_name') . '-socketio', Funcs::asset('widen/plugins/socketio/socket.io.min.js'), 9999, time(), ['in_footer' => 'true']);
+			wp_enqueue_script(Funcs::config('app.short_name') . '-live-reload', Funcs::asset('node/live-reload.js'), ['jquery'], time(), ['in_footer' => 'true']);
 		}
 
 //		wp_enqueue_script('dashboard');
