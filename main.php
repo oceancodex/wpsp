fcfd7ef96b80e5e6f56037eb783bdd146dcbd91c
Khánh Duy Nguyễn <khanhpkvn@gmail.com>
2026-06-09 20:04:10 +0700

    KDN-MAC-01: Update

--- main.php
+++ main.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name:         WordPress Starter Plugin
  * Description:         WordPress Starter Plugin - PHP ^8.3
- * Version:             13.0.4
+ * Version:             13.0.5
  * Requires at least:   6.4
  * Requires PHP:        8.3
  * Text Domain:         wpsp
