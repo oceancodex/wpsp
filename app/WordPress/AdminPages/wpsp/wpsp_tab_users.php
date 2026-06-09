f46af1d5784ce084d772244c500b14114f781d39
Khánh Duy Nguyễn <khanhpkvn@gmail.com>
2026-06-09 12:17:25 +0700

    KDN-PC-02: Update

--- app/WordPress/AdminPages/wpsp/wpsp_tab_users.php
+++ app/WordPress/AdminPages/wpsp/wpsp_tab_users.php
@@ -29,6 +29,9 @@ class wpsp_tab_users extends BaseAdminPage {
 	/**
 	 * Parent properties.
 	 */
+//	public $forceInit			   = false;
+//	public $forceInitSlug          = null;
+
 //	public $classes                = null;
 //	public $firstSubmenuTitle      = null;
 //	public $firstSubmenuClasses    = null;
