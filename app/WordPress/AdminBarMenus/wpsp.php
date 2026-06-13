63b62af0aa02c522881d4d98757a9bc25f4fe33c
Khánh Duy Nguyễn <khanhpkvn@gmail.com>
2026-06-13 19:24:18 +0700

    KDN-PC-01: Update DI, contructParams

--- app/WordPress/AdminBarMenus/wpsp.php
+++ app/WordPress/AdminBarMenus/wpsp.php
@@ -3,7 +3,7 @@
 namespace WPSP\App\WordPress\AdminBarMenus;
 
 use WPSP\App\Widen\Traits\InstancesTrait;
-use WPSPCORE\App\WordPress\UserMetaBoxes\PostTypeColumns\BaseAdminBarMenu;
+use WPSPCORE\App\WordPress\AdminBarMenus\BaseAdminBarMenu;
 
 class wpsp extends BaseAdminBarMenu {
 
