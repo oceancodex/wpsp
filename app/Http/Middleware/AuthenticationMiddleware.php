70b918c0e27e6597bd86e026017a07b535e7b322
Khánh Duy Nguyễn <khanhpkvn@gmail.com>
2026-07-01 21:03:22 +0700

    KDN-MAC-01: Update

--- app/Http/Middleware/AuthenticationMiddleware.php
+++ app/Http/Middleware/AuthenticationMiddleware.php
@@ -24,8 +24,8 @@ class AuthenticationMiddleware {
 			 * Nếu không kiểm tra path thì sẽ luôn bị redirect về trang login với bất cứ request nào.
 			 */
 			if (
-				(
-					is_admin() && (
+				(is_admin()
+					&& (
 						@preg_match('/page=' . Funcs::instance()->_regexPath($args['route']->path) . '$/iu', $requestPath)
 						|| @preg_match('/page=' . Funcs::instance()->_regexPath($args['route']->fullPathRegex) . '$/iu', $requestPath)
 					)
