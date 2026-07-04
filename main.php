627468ffa7b9b6fad22e4fa3dbbfb39807b16f31
Khánh Duy Nguyễn <khanhpkvn@gmail.com>
2026-07-04 20:12:24 +0700

    KDN-MAC-01: Update

--- main.php
+++ main.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name:         WPSP Framework - WordPress Starter Plugin
  * Description:         WPSP Framework - WordPress Starter Plugin - PHP ^8.3
- * Version:             13.1.34
+ * Version:             13.1.35
  * Requires at least:   6.4
  * Requires PHP:        8.3
  * Text Domain:         wpsp
@@ -19,5 +19,5 @@ if (!defined('ABSPATH')) {
 }
 
 if (!defined('WPSP_ARTISAN_START') && !defined('WPSP_ORIGINAL_WP')) {
-	require_once __DIR__ . '/bootstrap/plugin.php';
+	require_once __DIR__.'/bootstrap/plugin.php';
 }
