41bf86a61cee9fea33ba40dd55537bd8bb75e0eb
Khánh Duy Nguyễn <khanhpkvn@gmail.com>
2026-07-10 09:34:47 +0700

    KDN-PC-02: Update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,7 +6,9 @@
 
 	<title>{{ config('app.name', 'Laravel') }} {{ function_exists('get_bloginfo') ? '- ' . get_bloginfo('name') : '' }}</title>
 
-	@fonts
+	<!-- Fonts -->
+	<link rel="preconnect" href="https://fonts.bunny.net">
+	<link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
 
 	<!-- Styles / Scripts -->
 	<style>
