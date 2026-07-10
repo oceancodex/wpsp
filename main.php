c602d78e84f21d858f96de42cafd0717af1ecc84
Khánh Duy Nguyễn <khanhpkvn@gmail.com>
2026-07-10 09:33:44 +0700

    KDN-PC-02: Update

--- main.php
+++ main.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name:         WPSP Framework - WordPress Starter Plugin
  * Description:         WPSP Framework - WordPress Starter Plugin - PHP ^8.2
- * Version:             12.1.42
+ * Version:             12.1.43
  * Requires at least:   6.1
  * Requires PHP:        8.2
  * Text Domain:         wpsp
