5a4bcaf1abbf80918468a90e459beba8d049c51d
Khánh Duy Nguyễn <khanhpkvn@gmail.com>
2026-07-19 01:03:04 +0700

    KDN-PC-02: Update

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,8 @@
 console.log('Hello world');
+/**
+ * Echo exposes an expressive API for subscribing to channels and listening
+ * for events that are broadcast by Laravel. Echo and event broadcasting
+ * allow your team to quickly build robust real-time web applications.
+ */
+
+import './echo';
