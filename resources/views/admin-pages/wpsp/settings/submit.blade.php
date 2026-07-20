043f3fe8f0f7cd5c226cb5bfb4fc501e8af675a2
Khánh Duy Nguyễn <khanhpkvn@gmail.com>
2026-07-21 04:14:23 +0700

    KDN-PC-02: Update

--- resources/views/admin-pages/wpsp/settings/submit.blade.php
+++ resources/views/admin-pages/wpsp/settings/submit.blade.php
@@ -1,9 +1,9 @@
 <x-admin-pages.admin-page-meta-box id="submitdiv" :adminPageMetaBoxes="$admin_page_meta_boxes">
     <x-slot:title>Lưu thay đổi</x-slot:title>
     <x-slot:content>
-        <p class="px-3">Đây là meta box được xây dựng bằng Blade Template Engine và được gọi ra với phương thức "view".<br/>Khi sử dụng phương thức này, screen options sẽ cần được thiết lập để hiển thị ra.</p>
-        <div class="p-2 text-end bg-light">
-            <button type="submit" class="button button-primary">{{ wpsp_trans('messages.save_changes') }}</button>
-        </div>
+        <p class="mt-0">Đây là meta box được xây dựng bằng Blade Template Engine và được gọi ra với phương thức "view".<br/>Khi sử dụng phương thức này, screen options sẽ cần được thiết lập để hiển thị ra.</p>
     </x-slot:content>
+	<x-slot:footer>
+		<button type="submit" class="button button-primary">{{ wpsp_trans('messages.save_changes') }}</button>
+	</x-slot:footer>
 </x-admin-pages.admin-page-meta-box>
