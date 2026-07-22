eb30c7e1bcaa96876efa070e73f86c8e7d50143b
Khánh Duy Nguyễn <khanhpkvn@gmail.com>
2026-07-22 16:27:05 +0700

    KDN-PC-02: Update

--- app/WordPress/AdminPages/wpsp/wpsp_tab_settings.php
+++ app/WordPress/AdminPages/wpsp/wpsp_tab_settings.php
@@ -173,10 +173,10 @@ class wpsp_tab_settings extends BaseAdminPage {
 			],
 			'advanced' => [],
 			'closed' => [
-				'submitdiv' => true
+				'submitdiv' => true,
 			],
 			'hidden' => [
-				'testhiddendiv' => true
+				'testhiddendiv' => true,
 			],
 		];
 	}
@@ -195,7 +195,8 @@ class wpsp_tab_settings extends BaseAdminPage {
 
 	public function edit(Request $request, $id) {}
 
-	public function update(SettingsUpdateRequest $request) {
+	public function update(Request $request) {
+		dd($request->all());
 //		dd($this->request->route());
 
 //		try {
@@ -284,7 +285,7 @@ class wpsp_tab_settings extends BaseAdminPage {
 	public function scripts() {
 		wp_enqueue_media();
 		wp_enqueue_script(Funcs::config('app.short_name') . '-backend-admin', Funcs::asset('/ts/web/admin.min.js'), [
-			'jquery', 'jquery-ui-datepicker'
+			'jquery', 'jquery-ui-datepicker',
 		], Funcs::getVersion(), ['in_footer' => true]);
 		wp_enqueue_script(Funcs::config('app.short_name') . '-datepicker-vi', Funcs::asset('widen/plugins/datepicker/datepicker-vi.js'), null, Funcs::getVersion(), ['in_footer' => true]);
 	}
