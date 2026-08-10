8b4220b93e339072e3db8024b22f4bfadb65066d
Khánh Duy Nguyễn <khanhpkvn@gmail.com>
2026-08-10 20:01:25 +0700

    KDN-PC-02: Update

--- resources/views/components/admin-pages/common/popup.blade.php
+++ resources/views/components/admin-pages/common/popup.blade.php
@@ -13,13 +13,11 @@
 							</button>
 						</div>
 					</div>
-					<div class="inside w-auto mt-0" style="max-height: 950px; overflow: auto;">
+					<div class="inside w-auto pt-1" style="max-height: 950px; overflow: auto;">
 						{!! $content !!}
 					</div>
-					<div class="postbox-footer" style="padding: 12px;">
-						<div class="bg-light border-top text-end" style="padding: 12px;">
-							{!! $footer !!}
-						</div>
+					<div class="postbox-footer bg-light border-top text-end" style="padding: 12px;">
+						{!! $footer !!}
 					</div>
 				</div>
 			</div>
